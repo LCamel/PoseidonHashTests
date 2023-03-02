39934a3993075fe56ffe6e77e09dbfcc6092684a
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-03-03 00:37:49 +0800

    generate state.json for anvil

--- circomlibjs-0.0.8/DeployPosiedonHash.mjs
+++ circomlibjs-0.0.8/DeployPosiedonHash.mjs
@@ -3,6 +3,7 @@
 import { poseidon_gencontract } from "circomlibjs";
 import { ethers } from "ethers";
 import { poseidon } from "circomlibjs"; // for verifying only
+import * as fs from "fs";
 
 const provider = new ethers.providers.JsonRpcProvider(); // local
 
@@ -12,19 +13,20 @@ const signer = new ethers.Wallet(privKey, provider);
 
 var gases = [];
 var castCommands = [];
+var code = [];
 
 for (let nInputs = 1; nInputs <= 6; nInputs++) {
 
     console.log("\n\n==== nInputs: " + nInputs);
-    
+
     // the ABI has both bytes32 and uint256 declarations. we only need the later one
     const poseidonABI = poseidon_gencontract.generateABI(nInputs).slice(1, 2);
     const poseidonBytecode = poseidon_gencontract.createCode(nInputs);
-    
+
     console.log("==== ABI for uint256: ",
         new ethers.utils.Interface(poseidonABI).format(ethers.utils.FormatTypes.full));
     console.log("bytecode length: " + poseidonBytecode.length);
-    
+
     const factory = new ethers.ContractFactory(poseidonABI, poseidonBytecode, signer)
     const contract = await factory.deploy();
     console.log("will be deployed at addr: " + contract.address);
@@ -32,16 +34,17 @@ for (let nInputs = 1; nInputs <= 6; nInputs++) {
     //console.log("deployed.");
     const receipt = await contract.deployTransaction.wait();
     console.log("deployed. gasUsed: " + receipt.gasUsed.toBigInt());
-    
+    code[nInputs] = await provider.getCode(contract.address);
+
     const inputs = [1, 2, 3, 4, 5, 6].slice(0, nInputs);
     console.log("inputs: " + inputs);
-    
+
     // using the newly deployed contract
     const ans = await contract.poseidon(inputs);
     console.log("on-chain result : ", ans.toBigInt());
     // using javascript
     console.log("off-chain result: ", poseidon(inputs));
-    
+
     const gas = await contract.estimateGas.poseidon(inputs);
     console.log("estimated gas (21000 included): " + gas);
     gases.push(gas);
@@ -56,10 +59,23 @@ for (let nInputs = 1; nInputs <= 6; nInputs++) {
 console.log("\n\ngas:");
 console.log("nInputs\tgas\tg-21000\tavg");
 gases.map((gas, i) => {
-console.log(`${i + 1}\t${gas}\t${gas - 21000}\t${(gas - 21000) / (i + 1)}`);
+    console.log(`${i + 1}\t${gas}\t${gas - 21000}\t${(gas - 21000) / (i + 1)}`);
 });
 
 console.log("\n\ncast commands:");
 castCommands.map((c) => console.log(c));
 
 
+console.log("\n\ngenerating anvil state json (with fake addresses)");
+
+let state = { accounts: {}};
+for (let i = 1; i <= 6; i++) {
+    let acc = {
+        nonce: 1,
+        balance: "0x0",
+        code: code[i],
+        storage: {}
+    };
+    state.accounts[`0x111111112222222233333333444444440000000${i}`] = acc;
+}
+fs.writeFileSync("state.json", JSON.stringify(state, undefined, 4));
