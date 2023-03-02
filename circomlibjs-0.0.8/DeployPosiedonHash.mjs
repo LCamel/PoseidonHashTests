fc83145b49ae3f3933a2dba645178388a4e012e6
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-03-02 22:39:02 +0800

    add DeployPosiedonHash.mjs

---
+++ circomlibjs-0.0.8/DeployPosiedonHash.mjs
@@ -0,0 +1,65 @@
+// SEE: deploy-semaphore.ts from Semaphore
+
+import { poseidon_gencontract } from "circomlibjs";
+import { ethers } from "ethers";
+import { poseidon } from "circomlibjs"; // for verifying only
+
+const provider = new ethers.providers.JsonRpcProvider(); // local
+
+const privKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+const signer = new ethers.Wallet(privKey, provider);
+
+var gases = [];
+var castCommands = [];
+
+for (let nInputs = 1; nInputs <= 6; nInputs++) {
+
+    console.log("\n\n==== nInputs: " + nInputs);
+    
+    // the ABI has both bytes32 and uint256 declarations. we only need the later one
+    const poseidonABI = poseidon_gencontract.generateABI(nInputs).slice(1, 2);
+    const poseidonBytecode = poseidon_gencontract.createCode(nInputs);
+    
+    console.log("==== ABI for uint256: ",
+        new ethers.utils.Interface(poseidonABI).format(ethers.utils.FormatTypes.full));
+    console.log("bytecode length: " + poseidonBytecode.length);
+    
+    const factory = new ethers.ContractFactory(poseidonABI, poseidonBytecode, signer)
+    const contract = await factory.deploy();
+    console.log("will be deployed at addr: " + contract.address);
+    //await contract.deployed();
+    //console.log("deployed.");
+    const receipt = await contract.deployTransaction.wait();
+    console.log("deployed. gasUsed: " + receipt.gasUsed.toBigInt());
+    
+    const inputs = [1, 2, 3, 4, 5, 6].slice(0, nInputs);
+    console.log("inputs: " + inputs);
+    
+    // using the newly deployed contract
+    const ans = await contract.poseidon(inputs);
+    console.log("on-chain result : ", ans.toBigInt());
+    // using javascript
+    console.log("off-chain result: ", poseidon(inputs));
+    
+    const gas = await contract.estimateGas.poseidon(inputs);
+    console.log("estimated gas (21000 included): " + gas);
+    gases.push(gas);
+    console.log("                       -21000 : " + (gas - 21000));
+    console.log(`              then divded by ${nInputs}: ` + (gas - 21000) / nInputs);
+
+    const args = `${contract.address} 'poseidon(uint[${nInputs}])' '${JSON.stringify(inputs)}'`;
+    castCommands.push(`cast call ${args}`);
+    castCommands.push(`cast send --private-key ${privKey} ${args}`);
+}
+
+console.log("\n\ngas:");
+console.log("nInputs\tgas\tg-21000\tavg");
+gases.map((gas, i) => {
+console.log(`${i + 1}\t${gas}\t${gas - 21000}\t${(gas - 21000) / (i + 1)}`);
+});
+
+console.log("\n\ncast commands:");
+castCommands.map((c) => console.log(c));
+
+
